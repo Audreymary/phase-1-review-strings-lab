f57a6b09375c45f238e920dbcd94668f7324c390
Audreymary <audreymoraa006@gmail.com>
2024-09-23 11:57:44 +0300

    add index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+let companyName = 'Scuber';
+let mostProfitableNeighborhood = "Chelsea";
+let companyCeo = "Susan Smith";
+let currentUser = "John Doe"; // Replace "John Doe" with the actual current user's name
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, " + 'JOHN DOE !';
+
+let shortGreeting = "Welcome, " + currentUser.charAt(0) + "!";
